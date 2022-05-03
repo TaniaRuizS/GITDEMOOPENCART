d0cc4024768a7c6968e3ff6be02c585dbe9ab0ca
TaniaRuizS <truizs@choucairtesting.com>
2022-05-03 14:58:57 -0500

    Abrir la pagina

--- src/main/java/co.com.choucair.certification.opencart/tasks/OpenUp.java
+++ src/main/java/co.com.choucair.certification.opencart/tasks/OpenUp.java
@@ -1,10 +1,20 @@
 package co.com.choucair.certification.opencart.tasks;
 
-import net.serenitybdd.screenplay.Performable;
+import co.com.choucair.certification.opencart.userinterface.CartOpenPage;
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Open;
 
 public class OpenUp implements Task {
+    private CartOpenPage cartOpenPage;
 
-    public static Performable thePage() {
+    public static OpenUp thePage() {
+        return Tasks.instrumented(OpenUp.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(Open.browserOn(cartOpenPage));
     }
 }
--- src/main/java/co.com.choucair.certification.opencart/userinterface/CartOpenPage.java
+++ src/main/java/co.com.choucair.certification.opencart/userinterface/CartOpenPage.java
@@ -1,4 +1,8 @@
 package co.com.choucair.certification.opencart.userinterface;
 
-public class CartOpenPage {
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://demo.opencart.com/admin/index.php?route=common/login")
+public class CartOpenPage extends PageObject {
 }
